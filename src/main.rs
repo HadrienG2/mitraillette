68e566672f426529b679e82555a8551be469b2db
Hadrien Grasland <grasland@lal.in2p3.fr>
2019-08-02 15:23:13 +0200

    Simplification du code

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,35 @@ const NB_FACES : usize = 6;
 // Histogramme d'un jet de dé par face (nb de dés tombé sur chaque face)
 type HistogrammeFaces = [usize; NB_FACES];
 
+// On va compter les probas sur 64-bit au cas où, on diminuera si besoin
+type Probabilite = f64;
+
+// Si on l'ensemble des jets de dés possibles à N dés, on se retrouve avec une
+// table des choix face auxquels on peut se retrouver en lançant les dés, et
+// des probabilités associées.
+type ChoixEtProba = (Choix, Probabilite);
+
 
 fn main() {
     println!();
+
+    // Tout d'abord, pour chaque nombre de dés, on calcule face à quels choix
+    // on peut se retrouver, et avec quelle probabilité
+    let mut distribution_choix = Vec::new();
 
     // On étudie des lancers de 1 à 6 dés
     for nb_des in 1..=NB_DES_TOT {
         let nb_comb = NB_FACES.pow(nb_des as u32);
         println!("Nombre de lancers possibles à {} dés: {}", nb_des, nb_comb);
 
-        let mut val_totale = 0;
-        let mut nb_par_tirage: HashMap<Choix, u16> = HashMap::new();
-
         // On énumère tous les lancers possibles pour ce nombre de dés
+        let mut comptage_choix: HashMap<Choix, u16> = HashMap::new();
         for num_comb in 0..nb_comb {
-            let mut reste = num_comb;
-            let mut histo = [0; NB_FACES];
-
             // On énumère les faces en traitant la combinaison comme un nombre
             // en base NB_FACES (note: la face 1 est numérotée 0), et on calcule
             // l'histogramme du nombre de dés étant tombé sur chaque face.
-            /* print!("- Combinaison: "); */
+            let mut reste = num_comb;
+            let mut histo = [0; NB_FACES];
             for _ in 0..nb_des {
                 let idx_face = reste % NB_FACES;
                 histo[idx_face] += 1;
@@ -43,72 +51,47 @@ fn main() {
 
             // On énumère les combinaisons qu'on peut prendre
             let choix = choix::enumerer_choix(histo);
-            let nb = nb_par_tirage.entry(choix.clone()).or_insert(0);
-            *nb += 1;
 
-            // Supposons qu'on s'arrête là, combien gagne-t'on?
-            let valeur_max = choix.iter().map(|comb| comb.valeur()).max();
-            if let Some(valeur_max) = valeur_max {
-                val_totale += valeur_max;
-            }
-
-            // TODO: Calculer le score de l'interprétation de valeur maximale de
-            //       la main. En la sommant entre les différentes combinaisons
-            //       dans un accumulateur u64, on pourra alors déterminer
-            //       l'espérance de gain "à l'ordre 1", c'est à dire dans le
-            //       cadre d'une stratégie simpliste où on lance les dés une
-            //       seule fois et s'arrête immédiatement.
-            //
-            //       Cette espérance de gain approximative pourra ensuite
-            //       être raffinée en parcourant l'arbre de décision à
-            //       profondeur N, utilisant l'approximation d'ordre 1 comme
-            //       estimateur de l'espérance en fin de parcours. Par exemple,
-            //       on peut calculer l'espérance de gain à deux jets de dés en
-            //       calculant le gain attendu pour chaque décision possible
-            //       d'un tour réussi selon les dés qu'on choisit ou non de
-            //       prendre, en supposant que le jet des dés restants suit
-            //       l'approximation d'ordre 1.
-            //
-            //       Puisqu'un tour de mitraillette se termine quasiment
-            //       toujours en une dizaine de tours grand max, un raffinement
-            //       à ordre fini et pas trop grand devrait fournir une
-            //       excellente approximation de l'espérance de gain réelle et
-            //       donner une piste vers la stratégie optimale.
-
-            // Certains résultats peuvent se comptabiliser de plusieurs façons:
-            // - Une suite (500 points) contient un as et un cinq (150 points)
-            // - Une triple paire peut contenir un ou deux brelans si il y a des
-            //   paires identiques, et peut contenir un certain nombre de paires
-            //   de 1 ou de 5.
-            // - Un brelan peut contenir des 1 ou des 5, mais vaut toujours plus
-            //   que la somme de ses constituants car triple 1 = 1000 et pas 100
-
-            // Une triple paire contenant des brelans coûte parfois plus cher si
-            // on l'interprète en tant que brelan. En cas de double brelan,
-            // l'interprétation "brelan" est favorable sauf en cas de brelan
-            // de 2. En cas de brelan simple, l'interprétation "brelan" est
-            // équivalente (sur le seul critère du score) pour un brelan de 5
-            // et favorable pour un brelan de 6 ou de 1.
-
-            // Il s'agit ici d'une évaluation sur le seul critère du score
-            // maximal en ne relançant pas les dés. En ajoutant le critère du
-            // nombre de dés consommé, la situation deviendra plus complexe.
+            // On compte la fréquence de chaque situation...
+            let compte = comptage_choix.entry(choix.clone()).or_insert(0);
+            *compte += 1;
         }
 
-        // Nous pouvons maintenant énumérer les combinaisons possibles
-        println!("Tirages possibles: {}", nb_par_tirage.len());
-        for (tirage, &nb) in nb_par_tirage.iter() {
-            let prop = nb as f32 / nb_comb as f32;
-            println!("- {:?} (Proportion: {})", tirage, prop);
-        }
-
-        // ...ce qui permet de calculer la proportion de jets perdants
-        let prop_perdant = nb_par_tirage[&Choix::new()] as f32 / nb_comb as f32;
+        // ...dont on tire, paramètre important, la proportion de jets perdants
+        let prop_perdant = comptage_choix[&Choix::new()] as f32 / nb_comb as f32;
         println!("Proportion combinaisons perdantes: {}", prop_perdant);
 
-        // ...et l'espérance de gain à un jet de dé
-        let esperance_un_jet = val_totale as f32 / nb_comb as f32;
-        println!("Espérance de gain à un jet: {}", esperance_un_jet);
+        // Nous en tirons une table des choix face auxquels on peut se
+        // retrouver, avec la probabilité de chacun.
+        let norme = 1. / (nb_comb as Probabilite);
+        let choix_et_probas =
+            comptage_choix.into_iter()
+                          .map(|(tirage, nb)| (tirage, (nb as Probabilite) * norme))
+                          .collect::<Vec<ChoixEtProba>>();
+
+        // Nous pouvons maintenant énumérer les combinaisons possibles
+        println!("Tirages possibles: {}", choix_et_probas.len());
+        for (tirage, proba) in choix_et_probas.iter() {
+            println!("- {:?} (Proportion: {})", tirage, proba);
+        }
+
+        // Calculons l'espérance de gain à un jet de dés (on lance les dés et on
+        // s'arrête immédiatement en prenant son gain maximum)
+        let esperance_un_jet_2 : f64 =
+            choix_et_probas.iter()
+                .map(|(choix, proba)| {
+                    choix.iter()
+                         .map(|comb| comb.valeur())
+                         .max()
+                         .unwrap_or(0) as f64
+                    * proba
+                })
+                .sum();
+        println!("Espérance de gain à un jet: {}", esperance_un_jet_2);
+
+        // Gardons de côté les choix face auxquels on peut se retrouver (et leur
+        // proba) pour ce nombre de dés.
+        distribution_choix.push(choix_et_probas);
 
         // TODO: Calculer l'espérance de gain totale
 
